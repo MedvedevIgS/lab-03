1ce36832a48da47b11b8d9e88a82f7897478e0c2
MedvedevIS <igormedvedev@gmail.com>
2020-04-26 00:22:36 +0400

    code: добавление проверки массива из трёх разных, трех одинаковых и одного числа

--- lab03/test.cpp
+++ lab03/test.cpp
@@ -11,7 +11,35 @@ test_positive() {
     assert(max == 3);
 }
 
+void test_otricala()
+{
+    double min=0;
+    double max=0;
+    find_minmax({-1,-2,-3},min,max);
+    assert(max == -1);
+    assert(min == -3);
+}
+void test_same()
+{
+    double min = 0;
+    double max = 0;
+    find_minmax({3, 3, 3}, min, max);
+    assert(min == 3);
+    assert(max == 3);
+}
+
+void test_ones()
+{
+    double min = 0;
+    double max = 0;
+    find_minmax({2}, min, max);
+    assert(min == 2);
+    assert(max == 2);
+}
+
 int
 main() {
     test_positive();
+    test_ones();
+    test_same();
 }
