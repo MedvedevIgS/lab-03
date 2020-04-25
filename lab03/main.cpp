5f64009632ca8f432ddf1a0fce49dd15e4d648cd
MedvedevIS <igormedvedev@gmail.com>
2020-04-25 19:41:38 +0400

    code: замена текстовой реализации программы графической

--- lab03/main.cpp
+++ lab03/main.cpp
@@ -3,6 +3,27 @@
 
 using namespace std;
 
+void svg_begin(double width, double height)
+{
+    cout << "<?xml version='1.0' encoding='UTF-8'?>\n";
+    cout << "<svg ";
+    cout << "width='" << width << "' ";
+    cout << "height='" << height << "' ";
+    cout << "viewBox='0 0 " << width << " " << height << "' ";
+    cout << "xmlns='http://www.w3.org/2000/svg'>\n";
+}
+
+void svg_end()
+{
+    cout << "</svg>\n";
+}
+
+void show_histogram_svg(const vector<size_t>& bins)
+{
+    svg_begin(400, 300);
+    svg_end();
+}
+
 vector<double> input_numbers(size_t count)
 {
  vector<double> result(count);
@@ -101,6 +122,6 @@ int main()
     cerr << "Enter column count: ";
     cin >> bin_count;
     const auto bins = make_histogram(numbers, bin_count);
-    show_histogram_text(bins);
+    show_histogram_svg(bins);
     return 0;
 }
