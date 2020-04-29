2a09cfff5f579fb505ca1849ef32f3471357dab7
MedvedevIS <igormedvedev@gmail.com>
2020-04-29 20:46:05 +0400

    code: выделение функции make_histogram в histogram.cpp

--- lab03/main.cpp
+++ lab03/main.cpp
@@ -16,23 +16,6 @@ vector<double> input_numbers(size_t count)
  return result;
 }
 
-vector<size_t> make_histogram(vector<double> numbers, size_t bin_count)
-{
-    vector<size_t> bins(bin_count);
-    double max, min;
-    find_minmax(numbers, min, max);
-    for (double number : numbers)
-    {
-        size_t bin = (size_t)((number - min) / (max - min) * bin_count);
-        if (bin == bin_count)
-        {
-            bin--;
-        }
-        bins[bin]++;
-    }
-    return bins;
-}
-
 void show_histogram_text(vector<size_t> bins)
 {
   const size_t SCREEN_WIDTH = 80;
