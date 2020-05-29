80eab6b461925e1266d0b30f935d630f61dbe8f6
MedvedevIS <MedvedevIS@gmail.com>
2020-05-29 12:24:12 +0400

    code: новый параметр prompt

--- lab04/main.cpp
+++ lab04/main.cpp
@@ -22,19 +22,27 @@ vector<double> input_numbers(istream& in, size_t count)
 }
 
 Input
-read_input(istream& in) {
+read_input(istream& in,  bool prompt) {
     Input data;
-
-    cerr << "Enter number count: ";
     size_t number_count;
-    in >> number_count;
 
-    cerr << "Enter numbers: ";
-    data.numbers = input_numbers(in, number_count);
+     if (prompt)
+    {
+        cerr << "Enter number count: ";
+        in >> number_count;
 
-    cerr << "Enter column count: ";
-    cin >> data.bin_count;
+        cerr << "Enter numbers: ";
+        data.numbers = input_numbers(in, number_count);
 
+        cerr << "Enter bin count: ";
+        in >> data.bin_count;
+    }
+    else
+    {
+        in >> number_count;
+        data.numbers = input_numbers(in, number_count);
+        in >> data.bin_count;
+    }
     return data;
 }
 
@@ -100,7 +108,7 @@ vector<size_t> make_histogram(Input data)
 
 int main()
 {
-    const auto data = read_input(cin);
+    const auto data = read_input(cin, true);
     const auto bins = make_histogram(data);
     show_histogram_svg(bins, data.bin_count);
     return 0;
