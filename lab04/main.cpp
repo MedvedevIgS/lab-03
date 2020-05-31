c0451d85480ea38fbbbbfc15a1e4378bc64c4e8c
MedvedevIS <MedvedevIS@gmail.com>
2020-05-31 14:28:36 +0400

    code: вывод в стиле С

--- lab04/main.cpp
+++ lab04/main.cpp
@@ -5,6 +5,7 @@
 #include <curl/curl.h>
 #include <sstream>
 #include <string>
+#include <cstdio>
 
 using namespace std;
 
@@ -144,6 +145,11 @@ download(const string& address) {
 
 int main(int argc, char* argv[])
 {
+    const char* name = "Commander Shepard";
+    int year = 2154;
+    printf("%s was born in %d.\n", name, year);
+    printf("n = %08x\n", 0x1234567);
+    return 0;
     Input data;
     if (argc > 1)
     {
